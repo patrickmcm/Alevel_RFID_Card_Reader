a702f98fe9ead8cd6fabe4ddca86eebd6267442c
peanut1120 <happyguy564h@gmail.com>
2023-10-19 16:23:56 +0100

    setup basics for websockets on device

--- firmware/main/webSockets.h
+++ firmware/main/webSockets.h
@@ -1,8 +1,8 @@
 #ifndef WEB_SOCKETS_H
 #define WEB_SOCKETS_H
 
-#include <ArduinoWebsockets.h>
-
+#include <WebSocketsClient.h>
 
 void webSocketEvent(WStype_t type, uint8_t * payload, size_t length);
+void setupWebSockets();
 #endif
